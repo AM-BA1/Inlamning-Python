c5ffc60a63b1037f1d099fc774602f724cf6b701
AM-BA1 <ami.heato@gmail.com>
2024-11-15 08:19:26 -0800

    done

--- app.py
+++ app.py
@@ -12,10 +12,9 @@ def get_tasks():
     return all_tasks
 
 @app.route("/tasks", methods=["POST"])
-def add_task():
+def task_add():
     data = request.json
     
-    new_tasks = []
     for item in data:
         description = item.get("description")
         category = item.get("category")
@@ -31,11 +30,81 @@ def add_task():
             "status": "pending"
         }
         all_tasks.append(new_task)
-        new_tasks.append(new_task)
-
     with open("tasks.json", "w") as j:
         json.dump(all_tasks, j)
-    return {"tasks": new_tasks}, 201
+    return new_task, 201
+
+@app.route("/tasks/<int:id>", methods=["GET"])
+def task_get(id):
+    for task in all_tasks:
+        if task["id"] == id:
+            return task, 200
+
+@app.route("/tasks/<int:id>", methods=["DELETE"])
+def task_delete(id):
+    for i, task in enumerate(all_tasks):
+        if task["id"] == id:
+            all_tasks.pop(i)
+            with open("tasks.json", "w") as j:
+                json.dump(all_tasks, j)
+            return f"The task with id:{id} has been deleted!", 201
+    return f"The task with id:{id} was not found!", 404
+
+@app.route("/tasks/<int:id>", methods=["PUT"])
+def task_update(id):
+    data = request.json
+    for item in data:
+        description = item.get("description")
+        category = item.get("category")
+
+    for task in all_tasks:
+        if task["id"] == id:
+            task.update({f"id": id, "description": description, "category": category, "status": "pending"})
+            with open("tasks.json", "w") as j:
+                json.dump(all_tasks, j)
+            return task, 200
+    return f"The task with id:{id} was not found!", 404     
+
+@app.route("/tasks/<int:id>/complete", methods=["PUT"])
+def task_complete(id):
+    for task in all_tasks:
+        if task["id"] == id:
+            task.update({"status": "complete"})
+            with open("tasks.json", "w") as j:
+                json.dump(all_tasks, j)
+            return task, 200
+    return f"The task with id:{id} was not found!", 404
+
+@app.route("/tasks/categories", methods=["GET"])
+def categories_all():
+    all_cat = []
+    for task in all_tasks:
+        category = task.get("category")
+        if category not in all_cat:
+            all_cat.append(category)
+    return all_cat
+
+@app.route("/tasks/categories/<category_input>", methods=["GET"])
+def category_tasks(category_input):
+    cat_tasks = []
+    for task in all_tasks:
+        if task["category"] == category_input:
+            cat_tasks.append(task)
+    return cat_tasks, 200
+
+@app.route("/")
+def index():  
+    return render_template("index.html", title="Tasks", tasks=all_tasks), 200
+
+             
+            
+
+
+
+
+
+        
+
 
 
 
